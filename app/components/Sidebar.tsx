630f1803e3494768f6d5a368ab69c3d117cb878f
Damianvit <damianvitchib@gmail.com>
2024-04-24 11:09:54 +0100

    first commit

---
+++ app/components/Sidebar.tsx
@@ -0,0 +1,11 @@
+"use client";
+
+import react from "react";
+
+function Sidebar() {
+    return (
+        <div className="bg-gray-900 w-32 rounded-md border  p-2">Sidebar</div>
+    );
+}
+
+export default Sidebar;
